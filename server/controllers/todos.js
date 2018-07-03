89ccd50913decfe074879409bfd1f8222aec2bf4
Andrew Eames <aeames@CIT-SCCT12-1206.cs.dixie.edu>
2018-07-03 11:02:53 -0600

    Server routers and controllers.

---
+++ server/controllers/todos.js
@@ -0,0 +1,21 @@
+const controller = {
+	listTodos: (req, res) =>
+		Todo.find()
+			.then(todos => res.json(todos)),
+	createTodo: (req, res) =>
+		Todo.create({
+			title: req.body.title
+		})
+			.then(todo => res.status(201).json(todo)),
+	updateTodo: (req, res) =>
+		Todo.findById(req.params.id)
+			.then(todo => {
+				todo.completed = req.body.completed
+				todo.title = req.body.title
+				return todo.save()
+			})
+			.then(todo => res.json(todo)),
+	deleteTodo: (req, res) =>
+		Todo.findByIdAndRemove(req.params.id)
+			.then(() => res.status(204).send())
+}
---
+++ server/routes/todos.js
@@ -0,0 +1,9 @@
+const express = require('express')
+const router = express.Router()
+
+router.get('/', controller.listTodos)
+router.post('/', controller.createTodo)
+router.put('/:id', controller.updateTodo)
+router.delete('/:id', controller.deleteTodo)
+
+module.exports = router
